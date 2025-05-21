71528eaa3dbc288eb0c5d3f91da39f80407f2e37
ALUMNOS <ALUMNOS@P610>
2025-05-21 11:24:05 +0200

    algo

--- src/EXAMEN_21M_PRIMO/EXAMEN_21M_PRIMO.java
+++ src/EXAMEN_21M_PRIMO/EXAMEN_21M_PRIMO.java
@@ -1,5 +1,20 @@
 package EXAMEN_21M_PRIMO;
 
-public class EXAMEN_21M_PRIMO {
+import java.util.Scanner;
 
+public class EXAMEN_21M_PRIMO {
+	
+	public static void main(String args[]) {
+		
+		Scanner ent = new Scanner(System.in);
+		int numero =0;
+		
+		System.out.println("Buenos días Ceinmark, soy Carlos");
+		
+		System.out.println("Introduce un número para averiguar si es primo o no");
+		numero = Integer.parseInt(ent.nextLine());
+		
+		
+		
+	}
 }
