08f2501fe5a5fa7beddc71ba2ec47b5c1e0446e4
ALUMNOS <ALUMNOS@P610>
2025-05-21 11:33:14 +0200

    commit de rama1

---
+++ src/EXAMEN_21M_PRIMO/EsPrimo.java
@@ -0,0 +1,13 @@
+package EXAMEN_21M_PRIMO;
+
+public class EsPrimo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("El numero 4 no es primo"+ 
+				"\nLos divisores del numero 4 son: 1,2, y 4");
+		
+		
+	}
+
+}
